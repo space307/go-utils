1b5c60647a6d1a8bfbd99867cdc718203e4aafaf
avd <alexander.demalin@gmail.com>
2018-08-14 19:11:44 +0300

    database: add postgres tests.

--- database/database_test.go
+++ database/database_test.go
@@ -31,3 +31,31 @@ func TestMySQLDB(t *testing.T) {
 	require.Error(t, err)
 	require.True(t, IsErrorDuplicateKey(err), err.Error())
 }
+
+func TestPostgresDB(t *testing.T) {
+	cfg := &Config{
+		Addr:     "127.0.0.1:5432",
+		User:     "postgres",
+		Database: "db_test",
+		SSLMode:  "disable",
+	}
+	db, err := Init("postgres", cfg)
+	require.NoError(t, err)
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS t1(id integer,v integer,CONSTRAINT t1_pkey PRIMARY KEY (id),CONSTRAINT t1_v_key UNIQUE (v));")
+	require.NoError(t, err)
+
+	_, err = db.Exec("delete from t1")
+	require.NoError(t, err)
+
+	tx, err := db.StartTransaction()
+	require.NoError(t, err)
+	_, err = tx.Tx.Exec("insert into t1(id, v) values(0, 1)")
+	require.NoError(t, err)
+
+	err = tx.Tx.Commit()
+	require.NoError(t, err)
+
+	_, err = db.Exec("insert into t1(id, v) values(1, 1)")
+	require.Error(t, err)
+	require.True(t, IsErrorDuplicateKey(err), err.Error())
+}
