d3cb480215f3b1eda473c0b7c1b887045629bdb0
Andrey Kolomiets <pilotuser@gmail.com>
2019-02-07 12:58:52 +0300

    Query for ctx (#37)
    * add tracing.Database with context
    * add tracing.Database query/exec/queryrow inside transactions
    * add tests
    * update changelog
    * add database tracing comment
    * add getter for database.Database object
    and tests

---
+++ tracing/database.go
@@ -0,0 +1,158 @@
+package tracing
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/opentracing/opentracing-go/log"
+	"github.com/space307/go-utils/database"
+)
+
+// Database is a object extended database.Database struct
+// for use with context and tracing
+type Database struct {
+	extDB *database.Database
+}
+
+// Init creates a storage object based on a given config.
+func Init(driver string, config *database.Config) (*Database, error) {
+	extDB, err := database.Init(driver, config)
+	return &Database{extDB: extDB}, err
+}
+
+// GetExtDatabase getter for database.Database object
+func (d *Database) GetExtDatabase() *database.Database {
+	return d.extDB
+}
+
+// Exec function with context and create tracing span
+func (d *Database) Exec(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+	span, _ := d.createSpanFromContext(ctx, query)
+	defer span.Finish()
+
+	res, err := d.extDB.Exec(query, args...)
+
+	if err != nil {
+		span.LogFields(log.String("error", err.Error()))
+	}
+
+	return res, err
+}
+
+// QueryRow function with context and create tracing span
+func (d *Database) QueryRow(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+	span, _ := d.createSpanFromContext(ctx, query)
+	defer span.Finish()
+
+	rows, err := d.extDB.QueryRow(query, args...)
+	if err != nil {
+		span.LogFields(log.String("error", err.Error()))
+	}
+
+	return rows, err
+}
+
+// Query function with context and create tracing span
+func (d *Database) Query(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+	span, _ := d.createSpanFromContext(ctx, query)
+	defer span.Finish()
+
+	rows, err := d.extDB.Query(query, args...)
+	if err != nil {
+		span.LogFields(log.String("error", err.Error()))
+	}
+
+	return rows, err
+}
+
+// StartTransaction start transaction, start tracing span
+// and return context, TxConnection and error
+func (d *Database) StartTransaction(ctx context.Context) (context.Context, *database.TxConnection, error) {
+	span, ctx := d.createSpanFromContext(ctx, `startTransaction`)
+
+	tx, err := d.extDB.StartTransaction()
+	if err != nil {
+		span.LogFields(log.String("error", err.Error()))
+	}
+
+	return opentracing.ContextWithSpan(ctx, span), tx, err
+}
+
+// Rollback try to rollback transaction and close open span
+func (d *Database) Rollback(ctx context.Context, conn *database.TxConnection) error {
+	span := opentracing.SpanFromContext(ctx)
+	if span != nil {
+		defer span.Finish()
+	}
+
+	err := conn.Tx.Rollback()
+	if err != nil {
+		span.LogFields(log.String("error", err.Error()))
+	}
+
+	return err
+}
+
+// Commit try to commit transaction and close open span
+func (d *Database) Commit(ctx context.Context, conn *database.TxConnection) error {
+	span := opentracing.SpanFromContext(ctx)
+	if span != nil {
+		defer span.Finish()
+	}
+
+	err := conn.Tx.Commit()
+	if err != nil {
+		span.LogFields(log.String("error", err.Error()))
+	}
+
+	return err
+}
+
+// ExecInsideTransaction execute sql Exec function and create span
+func (d *Database) ExecInsideTransaction(ctx context.Context, conn *database.TxConnection, query string, args ...interface{}) (sql.Result, error) {
+	span, _ := d.createSpanFromContext(ctx, query)
+	defer span.Finish()
+
+	rows, err := conn.Tx.Exec(query, args...)
+	if err != nil {
+		span.LogFields(log.String("error", err.Error()))
+	}
+
+	return rows, err
+}
+
+// QueryInsideTransaction execute sql Query function and create span
+func (d *Database) QueryInsideTransaction(ctx context.Context, conn *database.TxConnection, query string, args ...interface{}) (*sql.Rows, error) {
+	span, _ := d.createSpanFromContext(ctx, query)
+	defer span.Finish()
+
+	rows, err := conn.Tx.Query(query, args...)
+	if err != nil {
+		span.LogFields(log.String("error", err.Error()))
+	}
+
+	return rows, err
+}
+
+// QueryInsideTransaction execute sql QueryRow function and create span
+func (d *Database) QueryRowInsideTransaction(ctx context.Context, conn *database.TxConnection, query string, args ...interface{}) *sql.Row {
+	span, _ := d.createSpanFromContext(ctx, query)
+	defer span.Finish()
+
+	return conn.Tx.QueryRow(query, args...)
+}
+
+func (d *Database) createSpanFromContext(ctx context.Context, query string) (opentracing.Span, context.Context) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, query)
+
+	ext.PeerService.Set(span, `db`)
+	ext.PeerAddress.Set(span, d.extDB.GetConfig().Addr)
+	ext.SpanKind.Set(span, ext.SpanKindRPCServerEnum)
+	ext.DBStatement.Set(span, query)
+	ext.DBType.Set(span, `sql`)
+	ext.DBUser.Set(span, d.extDB.GetConfig().User)
+
+	return span, ctx
+}
