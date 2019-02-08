6b557ffb7e18de171511bf17f090ee6e4cee6103
Andrey Bosonchenko <andboson@gmail.com>
2019-02-08 12:44:57 +0200

    added check iface converson

--- amqp-kit/tracing.go
+++ amqp-kit/tracing.go
@@ -12,14 +12,17 @@ import (
 func TraceEndpoint(tracer opentracing.Tracer, operationName string) endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (interface{}, error) {
-			spCtx := ctx.Value(ampqCtx).(*opentracing.SpanContext)
-			sp := tracer.StartSpan(operationName, opentracing.FollowsFrom(*spCtx))
-			defer sp.Finish()
+			var sp opentracing.Span
+			if spCtx, ok := ctx.Value(ampqCtx).(*opentracing.SpanContext); ok {
+				sp = tracer.StartSpan(operationName, opentracing.FollowsFrom(*spCtx))
+				defer sp.Finish()
 
-			otext.SpanKindRPCServer.Set(sp)
-			ctx = opentracing.ContextWithSpan(ctx, sp)
+				otext.SpanKindRPCServer.Set(sp)
+				ctx = opentracing.ContextWithSpan(ctx, sp)
+			}
+
 			i, err := next(ctx, request)
-			if err != nil {
+			if err != nil && sp != nil {
 				sp.SetTag("error", err.Error())
 			}
 
